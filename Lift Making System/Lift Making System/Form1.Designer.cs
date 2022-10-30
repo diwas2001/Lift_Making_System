98b2e62a1cfe0b6dc318c005683a3532a180aa1c
diwas2001 <suyaldiwas11@gmail.com>
2022-10-30 21:48:44 +0545

    lift control basic

--- Lift Making System/Lift Making System/Form1.Designer.cs
+++ Lift Making System/Lift Making System/Form1.Designer.cs
@@ -29,21 +29,38 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Lift = new System.Windows.Forms.PictureBox();
             this.buttonUp = new System.Windows.Forms.Button();
             this.buttonDown = new System.Windows.Forms.Button();
             this.timerUp = new System.Windows.Forms.Timer(this.components);
+            this.timerDown = new System.Windows.Forms.Timer(this.components);
+            this.Left_Door = new System.Windows.Forms.PictureBox();
+            this.Right_Door = new System.Windows.Forms.PictureBox();
+            this.timerOpen = new System.Windows.Forms.Timer(this.components);
+            this.timerClose = new System.Windows.Forms.Timer(this.components);
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.First_Floor_Left = new System.Windows.Forms.PictureBox();
+            this.First_Floor_Right = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.Lift)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Left_Door)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Right_Door)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.First_Floor_Left)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.First_Floor_Right)).BeginInit();
             this.SuspendLayout();
             // 
             // Lift
             // 
             this.Lift.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Lift.Location = new System.Drawing.Point(130, 385);
+            this.Lift.Image = ((System.Drawing.Image)(resources.GetObject("Lift.Image")));
+            this.Lift.Location = new System.Drawing.Point(171, 385);
             this.Lift.Name = "Lift";
-            this.Lift.Size = new System.Drawing.Size(125, 246);
+            this.Lift.Size = new System.Drawing.Size(183, 247);
+            this.Lift.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Lift.TabIndex = 0;
             this.Lift.TabStop = false;
+            this.Lift.Click += new System.EventHandler(this.Lift_Click);
             // 
             // buttonUp
             // 
@@ -63,22 +80,109 @@
             this.buttonDown.TabIndex = 2;
             this.buttonDown.Text = "Down";
             this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
             // 
             // timerUp
             // 
             this.timerUp.Tick += new System.EventHandler(this.timerUp_Tick);
+            // 
+            // timerDown
+            // 
+            this.timerDown.Tick += new System.EventHandler(this.timerDown_Tick);
+            // 
+            // Left_Door
+            // 
+            this.Left_Door.BackColor = System.Drawing.Color.Silver;
+            this.Left_Door.Image = ((System.Drawing.Image)(resources.GetObject("Left_Door.Image")));
+            this.Left_Door.Location = new System.Drawing.Point(171, 385);
+            this.Left_Door.Name = "Left_Door";
+            this.Left_Door.Size = new System.Drawing.Size(88, 247);
+            this.Left_Door.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Left_Door.TabIndex = 3;
+            this.Left_Door.TabStop = false;
+            // 
+            // Right_Door
+            // 
+            this.Right_Door.BackColor = System.Drawing.Color.Silver;
+            this.Right_Door.Image = ((System.Drawing.Image)(resources.GetObject("Right_Door.Image")));
+            this.Right_Door.Location = new System.Drawing.Point(258, 385);
+            this.Right_Door.Name = "Right_Door";
+            this.Right_Door.Size = new System.Drawing.Size(96, 247);
+            this.Right_Door.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Right_Door.TabIndex = 4;
+            this.Right_Door.TabStop = false;
+            // 
+            // timerOpen
+            // 
+            this.timerOpen.Tick += new System.EventHandler(this.timerOpen_Tick);
+            // 
+            // timerClose
+            // 
+            this.timerClose.Tick += new System.EventHandler(this.timerClose_Tick);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(409, 456);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(94, 29);
+            this.buttonOpen.TabIndex = 5;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(599, 446);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(115, 29);
+            this.buttonClose.TabIndex = 6;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // First_Floor_Left
+            // 
+            this.First_Floor_Left.BackColor = System.Drawing.Color.Silver;
+            this.First_Floor_Left.Image = ((System.Drawing.Image)(resources.GetObject("First_Floor_Left.Image")));
+            this.First_Floor_Left.Location = new System.Drawing.Point(171, 85);
+            this.First_Floor_Left.Name = "First_Floor_Left";
+            this.First_Floor_Left.Size = new System.Drawing.Size(88, 247);
+            this.First_Floor_Left.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.First_Floor_Left.TabIndex = 7;
+            this.First_Floor_Left.TabStop = false;
+            // 
+            // First_Floor_Right
+            // 
+            this.First_Floor_Right.BackColor = System.Drawing.Color.Silver;
+            this.First_Floor_Right.Image = ((System.Drawing.Image)(resources.GetObject("First_Floor_Right.Image")));
+            this.First_Floor_Right.Location = new System.Drawing.Point(258, 85);
+            this.First_Floor_Right.Name = "First_Floor_Right";
+            this.First_Floor_Right.Size = new System.Drawing.Size(96, 247);
+            this.First_Floor_Right.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.First_Floor_Right.TabIndex = 8;
+            this.First_Floor_Right.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 665);
+            this.Controls.Add(this.First_Floor_Right);
+            this.Controls.Add(this.First_Floor_Left);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.Right_Door);
+            this.Controls.Add(this.Left_Door);
             this.Controls.Add(this.buttonDown);
             this.Controls.Add(this.buttonUp);
             this.Controls.Add(this.Lift);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.Lift)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Left_Door)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Right_Door)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.First_Floor_Left)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.First_Floor_Right)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -89,5 +193,14 @@
         private Button buttonUp;
         private Button buttonDown;
         private System.Windows.Forms.Timer timerUp;
+        private System.Windows.Forms.Timer timerDown;
+        private PictureBox Left_Door;
+        private PictureBox Right_Door;
+        private System.Windows.Forms.Timer timerOpen;
+        private System.Windows.Forms.Timer timerClose;
+        private Button buttonOpen;
+        private Button buttonClose;
+        private PictureBox First_Floor_Left;
+        private PictureBox First_Floor_Right;
     }
 }
--- Lift Making System/Lift Making System/Form1.cs
+++ Lift Making System/Lift Making System/Form1.cs
@@ -9,20 +9,115 @@ namespace Lift_Making_System
 
         private void timerUp_Tick(object sender, EventArgs e)
         {
-          
-                Lift.Location = new Point(Lift.Location.X, Lift.Location.Y - 10);
-                timerUp.Enabled = true;
-                if (Lift.Location.Y == 85)
-                {
-                    timerUp.Enabled = false;
-                }
-            
-           
+
+            Lift.Location = new Point(Lift.Location.X, Lift.Location.Y - 10);
+            timerUp.Enabled = true;
+            if (Lift.Location.Y == 85)
+            {
+                timerUp.Enabled = false;
+            }
+
+
         }
 
         private void buttonUp_Click(object sender, EventArgs e)
         {
             timerUp.Enabled = true;
+        }
+
+        private void timerDown_Tick(object sender, EventArgs e)
+        {
+            Lift.Location = new Point(Lift.Location.X, Lift.Location.Y + 10);
+            timerDown.Enabled = true;
+            if (Lift.Location.Y == 385)
+            {
+                timerDown.Enabled = false;
+            }
+
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            timerDown.Enabled = true;
+        }
+
+        private void Lift_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timerOpen.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timerClose.Enabled = true;
+        }
+
+        private void timerOpen_Tick(object sender, EventArgs e)
+        {
+            if(Lift.Location.Y == 385)
+            {
+                Left_Door.Location = new Point(Left_Door.Location.X - 10, Left_Door.Location.Y);
+                Right_Door.Location = new Point(Right_Door.Location.X + 10, Right_Door.Location.Y);
+                timerOpen.Enabled = true;
+                if (Left_Door.Location.X == 101)
+                {
+                    timerOpen.Enabled = false;
+
+
+                }
+            }
+            if(Lift.Location.Y == 85)
+            {
+               First_Floor_Left.Location = new Point(First_Floor_Left.Location.X - 10, First_Floor_Left.Location.Y);
+                First_Floor_Right.Location = new Point(First_Floor_Right.Location.X + 10, First_Floor_Right.Location.Y);
+                timerOpen.Enabled = true;
+                if (First_Floor_Left.Location.X == 101)
+                {
+                    timerOpen.Enabled = false;
+
+
+                }
+            }
+           
+        }
+
+        private void timerClose_Tick(object sender, EventArgs e)
+        {
+            if(Lift.Location.Y == 385)
+            {
+                Left_Door.Location = new Point(Left_Door.Location.X + 10, Left_Door.Location.Y);
+                Right_Door.Location = new Point(Right_Door.Location.X - 10, Right_Door.Location.Y);
+                timerClose.Enabled = true;
+                if (Left_Door.Location.X == 171)
+                {
+                    timerClose.Enabled = false;
+
+
+                }
+            }
+            if (Lift.Location.Y == 85)
+            {
+
+                First_Floor_Left.Location = new Point(First_Floor_Left.Location.X + 10, First_Floor_Left.Location.Y);
+                First_Floor_Right.Location = new Point(First_Floor_Right.Location.X - 10, First_Floor_Right.Location.Y);
+                timerClose.Enabled = true;
+                if (First_Floor_Left.Location.X == 171)
+                {
+                    timerClose.Enabled = false;
+
+
+                }
+            }
+
         }
     }
 }
